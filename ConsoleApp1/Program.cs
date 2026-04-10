aa9141088b0db19dc50617702ba36f793d470c3a
angela chaj <angelachaj648@gmail.com>
2026-04-10 11:08:55 -0600

    almacene los numeros en el primer arrglos como numeros

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Solicitar al usuario la cantidad n de números enteros y almacenarlos en un arreglo. Luego, recorrer el arreglo para calcular el promedio general. Después, mostrar en pantalla el promedio y listar únicamente los valores que están por encima del promedio. Finalmente, indicar cuántos números cumplen esa condición.
+
+Console.Write("Ingrese la cantidad de números: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] numeros = new int[n];
+int suma = 0;
+
+
+for (int i = 0; i < n; i++)
+{
+	Console.Write($"Número {i + 1}: ");
+	numeros[i] = int.Parse(Console.ReadLine());
+	suma += numeros[i];
+}
+
+
+double promedio = (double)suma / n;
+
+
